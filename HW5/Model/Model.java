d9751e1626ed7ac41b7d1f35bd12a9470117c325
Don-CTACOH <115583310+Don-CTACOH@users.noreply.github.com>
2023-06-22 18:53:06 +0300

    Create Model.java

---
+++ HW5/Model/Model.java
@@ -0,0 +1,23 @@
+package OOP_Java.HW5.Model;
+
+import OOP_Java.HW5.Controller.iGetModel;
+
+import java.util.List;
+import java.util.Map;
+
+public class Model implements iGetModel {
+    private List<Student1> student1List;
+    public Model(List<Student1> student1List){
+        this.student1List = student1List;
+    }
+
+    @Override
+    public Map<Long, Student1> getAllHashMapStudent() {
+        return null;
+    }
+
+    @Override
+    public String deleteStudentBuyID() {
+return null;
+    }
+}

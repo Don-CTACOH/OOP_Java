21be8f71437a2eace9f9e99a697a2351565e7a43
Don-CTACOH <115583310+Don-CTACOH@users.noreply.github.com>
2023-06-27 17:51:29 +0300

    Create TeacherService.java

---
+++ HW3_4/SyudentServise/TeacherService.java
@@ -0,0 +1,30 @@
+package OOP_Java.HW3_4.SyudentServise;
+
+import OOP_Java.HW3_4.StudentDomen.Student;
+import OOP_Java.HW3_4.StudentDomen.Teacher;
+
+import java.util.ArrayList;
+import java.util.List;
+// создадим сервис работ с учителями
+public class TeacherService implements iPersonService<Teacher>{
+    private int count;
+    private String acadDegree;
+    private List<Teacher> teachers;
+
+    public TeacherService() {
+        this.teachers = new ArrayList<Teacher>();
+    }
+    @Override
+    public List<Teacher> getAll() {
+        return teachers;
+    }
+
+
+
+    @Override
+    public void create(String firstName, String secondName, int age) {
+        Teacher teach = new Teacher(firstName,secondName,age,count,acadDegree);
+        count++;
+        teachers.add(teach);
+    }
+}

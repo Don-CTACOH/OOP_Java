609f7f8dac46be9b9763cb70ea4142bec401c4cc
Don-CTACOH <115583310+Don-CTACOH@users.noreply.github.com>
2023-06-27 14:14:51 +0300

    Update iPersonController.java

--- HW3&4/Controllers/iPersonController.java
+++ HW3&4/Controllers/iPersonController.java
@@ -1,6 +1,6 @@
-package OOP_Java.HW3&4.Controllers;
+package OOP_Java.HW3_4.Controllers;
 
-import OOP_Java.HW3&4.StudentDomen.Person;
+import OOP_Java.HW3_4.StudentDomen.Person;
 // создадим интерфейс основного контроллера работы с персоналом
 public interface iPersonController<T extends Person> {
     void create(String firstName, String secondName, int age);

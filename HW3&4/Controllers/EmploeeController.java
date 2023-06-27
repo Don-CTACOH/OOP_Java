845ff8e85a4185d08d1975eb2487a35284bb8986
Don-CTACOH <115583310+Don-CTACOH@users.noreply.github.com>
2023-06-27 12:47:54 +0300

    Create EmploeeController.java

---
+++ HW3&4/Controllers/EmploeeController.java
@@ -0,0 +1,16 @@
+package OOP_Java.HW3&4.Controllers;
+
+import OOP_Java.HW3&4.StudentDomen.Emploee;
+import OOP_Java.HW3&4.SyudentServise.EmploeeService;
+// создадим контроллер выполнения логики работ работников
+public class EmploeeController implements iPersonController<Emploee>{
+    private final EmploeeService empService = new EmploeeService();
+    @Override
+    public void create(String firstName, String secondName, int age) {
+        empService.create(firstName, secondName, age);
+    }
+    static public <T extends Emploee> void paySalary(T person){
+        System.out.println(person.getFirstName() + " выплачена зарплата 50000р");
+    }
+
+}

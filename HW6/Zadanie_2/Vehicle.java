6aa5a544b74c797032b3ab2ccdd938f5bf8bfe8a
Don-CTACOH <115583310+Don-CTACOH@users.noreply.github.com>
2023-06-28 23:18:18 +0300

    Create Vehicle.java

---
+++ HW6/Zadanie_2/Vehicle.java
@@ -0,0 +1,40 @@
+package OOP_Java.HW6.Zadanie_2;
+/*2) Переписать код SpeedCalculation в соответствии с Open-Closed Principle:
+public class SpeedCalculation {
+    public double calculateAllowedSpeed(Vehicle vehicle) {
+        if (vehicle.getType().equalsIgnoreCase("Car")) {
+            return vehicle.getMaxSpeed() * 0.8;
+        } else if (vehicle.getType().equalsIgnoreCase("Bus")) {
+            return vehicle.getMaxSpeed() * 0.6;
+        }
+​
+        return 0.0;
+    }
+}
+public class Vehicle {
+    int maxSpeed;
+    String type;
+    public Vehicle(int maxSpeed, String type) {
+        this.maxSpeed = maxSpeed;
+        this.type = type;
+    }
+    public int getMaxSpeed() {
+        return this.maxSpeed;
+    }
+    public String getType() {
+        return this.type;
+    }
+}*/
+
+// Решение: Создал абстрактный класс, к нему наследуем потомков с переопределеными методами по вычислению скорости
+public abstract class Vehicle {
+    int maxSpeed;
+
+    public Vehicle(int maxSpeed, String type) {
+        this.maxSpeed = maxSpeed;
+    }
+    public int getMaxSpeed() {
+        return this.maxSpeed;
+    }
+    public abstract double calculateAllowedSpeed(Vehicle vehicle);
+}
